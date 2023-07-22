a799b67b2eae481f36f1438aa260f64f0cb17e25
kr-pound <79203105+kr-pound@users.noreply.github.com>
2023-07-22 21:28:38 +0700

    Feat: add objects examples

---
+++ objects/1-stopwatch.js
@@ -0,0 +1,47 @@
+function Stopwatch() {
+    let running = false;
+
+    let startTime = null;
+    let stopTime = null;
+
+    let duration = 0;
+
+    this.start = function() {
+        if (running)
+            throw new Error('Stopwatch has already started');
+        running = true;
+        startTime = new Date();
+    };
+
+    this.stop = function() {
+        if (!running)
+            throw new Error('Stopwatch is not started');
+        running = false;
+        stopTime = new Date();
+
+        const seconds = (stopTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+
+    this.reset = function() {
+        running = false;
+        startTime = null;
+        stopTime = null;
+        duration = 0;
+    };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return duration;
+        }
+    });
+}
+
+const sw = new Stopwatch();
+
+console.log(sw.duration);
+sw.start();
+//sw.start();
+
+sw.stop();
+//sw.stop();
